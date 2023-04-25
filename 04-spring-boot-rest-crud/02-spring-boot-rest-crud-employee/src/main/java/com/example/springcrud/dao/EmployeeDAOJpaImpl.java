2b376606c0e10b7187dc3f5a4fc6f6bcb142a7df
Tom <pjoh1092@gmail.com>
2023-04-25 15:17:48 -0500

    add service layer

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/springcrud/dao/EmployeeDAOJpaImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/springcrud/dao/EmployeeDAOJpaImpl.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
@@ -22,5 +21,23 @@ public class EmployeeDAOJpaImpl implements EmployeeDAO{
         TypedQuery<Employee> theQuery = entityManager.createQuery("from Employee", Employee.class);
         List<Employee> employees = theQuery.getResultList();
         return employees;
+    }
+
+    @Override
+    public Employee findById(int theId){
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        return theEmployee;
+    }
+
+    @Override
+    public Employee save(Employee theEmployee) {
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        return dbEmployee;
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Employee theEmployee = entityManager.find(Employee.class, theId);
+        entityManager.remove(theEmployee);
     }
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/springcrud/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/example/springcrud/service/EmployeeService.java
@@ -1,13 +1,12 @@
-package com.example.springcrud.dao;
+package com.example.springcrud.service;
 
 import com.example.springcrud.entity.Employee;
 
 import java.util.List;
 
-public interface EmployeeDAO {
+public interface EmployeeService {
     List<Employee> findAll();
     Employee findById(int theId);
     Employee save(Employee theEmployee);
     void deleteById(int theId);
-
 }
