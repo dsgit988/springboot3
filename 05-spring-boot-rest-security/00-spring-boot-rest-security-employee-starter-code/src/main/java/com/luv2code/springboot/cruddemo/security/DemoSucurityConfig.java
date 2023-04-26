66ed7fe1857860dccb06f0ea9189306f14597de9
Tom <pjoh1092@gmail.com>
2023-04-26 15:13:43 -0500

    REST API SECURITY CUSTOM TABLES

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSucurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSucurityConfig.java
@@ -7,10 +7,45 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DemoSucurityConfig {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from members where user_id=?"
+        );
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+        return jdbcUserDetailsManager;
+    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+               configurer
+                       .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                       .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                       .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                       .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                       .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+
+        );
+        http.httpBasic();
+        http.csrf().disable();
+        return http.build();
+    }
+
+
+        /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
         UserDetails john = User.builder()
@@ -33,19 +68,6 @@ public class DemoSucurityConfig {
 
         return new InMemoryUserDetailsManager(john,mary,susan);
     }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests(configurer ->
-               configurer
-                       .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
-                       .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
-                       .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
-                       .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
-                       .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
 
-        );
-        http.httpBasic();
-        http.csrf().disable();
-        return http.build();
-    }
+     */
 }
