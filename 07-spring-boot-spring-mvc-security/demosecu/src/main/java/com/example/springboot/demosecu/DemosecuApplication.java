6268b340868c62ecc4f27b2622922a67b32e3657
Tom <pjoh1092@gmail.com>
2023-04-27 13:13:09 -0500

    spring mvc

---
+++ 07-spring-boot-spring-mvc-security/demosecu/src/main/java/com/example/springboot/demosecu/DemosecuApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.demosecu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosecuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosecuApplication.class, args);
+	}
+
+}
